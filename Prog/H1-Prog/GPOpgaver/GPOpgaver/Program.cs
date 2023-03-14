ae72ba6a2ee67d9efb40a9e9fc21cc308ce2adbc
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-03-14 08:26:10 +0100

    New set of assignments

---
+++ Prog/H1-Prog/GPOpgaver/GPOpgaver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GPOpgaver
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Opgaver.IsPalindrome("homoh"));
+            Console.WriteLine(Opgaver.IsPalindrome("kayak"));
+
+            Console.ReadLine();
+        }
+    }
+}
