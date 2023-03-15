05939b4c93b325219aba881e3df4055c4b3fd129
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-03-15 12:08:42 +0100

    ad

--- DatabaseServer/DBtesting/DBtesting/Program.cs
+++ DatabaseServer/DBtesting/DBtesting/Program.cs
@@ -1,15 +1,16 @@
 ﻿using TECHCOOL;
 
-SQLet.ConnectSqlServer("H1PD021123_Jokes", "docker.data.techcollege.dk", "h1pd021123", "h1pd021123");
+//SQLet.ConnectSqlServer("H1PD021123_Jokes", "docker.data.techcollege.dk", "h1pd021123", "h1pd021123");
+SQLet.ConnectSQLite(@"c:\users\Jacob Ø. Andersen\documents\mydb.db");
 Console.WriteLine("Connected to server");
 
 //SQLet.Execute("INSERT INTO jokes (joke) VALUES ('Haha a joke')");
 
-var res = SQLet.GetResult("SELECT joke FROM jokes");
+var res = SQLet.GetResult("SELECT name FROM groups");
 
 foreach (var item in res)
 {
-    Console.WriteLine(item["joke"]);
+    Console.WriteLine(item["name"]);
 }
 
 Console.WriteLine("Done");
